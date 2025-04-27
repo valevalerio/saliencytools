eb2fe0befc2aee47d89b6ff95bf1662e7439f548
valevalerio <valerio12bonsignori@gmail.com>
2025-04-27 16:53:01 +0200

    toward the pip package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SaliencyMetrics",
+    version="0.1.0",
+    author="Valerio Bonsignori",
+    author_email="valerio.bonsignori@phd.unipi.it",
+    description="A collection of metrics to compare saliency maps, validated using KNN-like classifiers on MNIST.",
+    long_description= open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/valevalerio/SaliencyMetrics",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        "numpy==1.24.4",
+        "pandas==1.5.3",
+        "matplotlib==3.7.5",
+        "seaborn==0.13.2",
+        "scikit-learn==1.3.2",
+        "scikit-multilearn==0.2.0",
+        "scipy==1.10.1",
+    ],
+)
---
+++ test/test_metrics.py
@@ -0,0 +1,7 @@
+""" This Module is used to check the coherency of the metrics implemented in the project."""
+
+from ..SaliencyMetrics.metrics import *
+
+def test_metrics():
+    
+    
